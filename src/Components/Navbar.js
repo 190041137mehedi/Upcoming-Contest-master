dbb678e776603e34d81efc8e38196be2f6ec00ec
190041137mehedI <190041137mehedi@gmail.com>
2023-06-01 22:11:44 +0600

    New

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faEnvelope, faCog } from '@fortawesome/free-solid-svg-icons';
-
 import '../Components/Navbar.css';
 
 // Add the icons to the library
